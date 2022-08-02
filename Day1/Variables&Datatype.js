35ddc1f32dc79523e298520a34077dc7cf72d38b
SoundappanR <soundappanravi1999@gmail.com>
2022-08-02 23:16:53 +0530

    Day 1 variables and datatype

---
+++ Day1/Variables&Datatype.js
@@ -0,0 +1,24 @@
+// Variables and Datatype
+
+// Variables are use dto store data
+
+// Datatypes --> 1) Primitive/Value  2) Referrence
+
+// Primitive datatype / value datatype ==> string , boolean, number , undefined, nulls
+
+// Referrence datatype ==> Objects , Arrays , Function
+
+var fname = "Wolf";
+var age = 23;
+var price = 100.001;
+var isEligible = true;
+isEligible = false; // re-assign
+var a;
+const pi = 3.14; // We cannot re-assign const
+
+console.log(fname, typeof fname);
+console.log(age);
+console.log(price);
+console.log(isEligible);
+console.log(a, typeof a);
+console.log(pi, typeof pi);
