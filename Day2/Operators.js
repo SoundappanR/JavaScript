708845efec918b9f41ef356bfe69b7b8c3828492
SoundappanR <soundappanravi1999@gmail.com>
2022-08-03 19:03:48 +0530

    Day2 Operators and.. static and dynamic datatype

---
+++ Day2/Operators.js
@@ -0,0 +1,18 @@
+// Operators
+
+//Arithmetic Operator
+let a = 1;
+let b = 2;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b); // Value will be in decimal format
+console.log(10 % 3); // remainder value
+console.log(2 ** 2); // Power
+
+//Assignment Operator
+// =
+// assign a value to a variable. Eg -> let a = 1;
+let age = 10;
+age += 15; // age = age + 15; same way we can use for sub , div , multiply , modulus , power
+console.log(age);
